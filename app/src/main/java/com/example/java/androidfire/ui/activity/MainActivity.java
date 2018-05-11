77576814b5e63023f55a3b1cb87e8892ac0d794a
sunhesong <1978086657@qq.com>
2018-05-11 09:11:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/java/androidfire/ui/activity/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.java.androidfire.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
+
+import com.example.java.androidfire.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout re;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim);
+        re.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(MainActivity.this, ZhuActivity.class);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+
+    private void initView() {
+        re = (RelativeLayout) findViewById(R.id.re);
+    }
+}
---
+++ app/src/main/java/com/example/java/androidfire/ui/activity/ZhuActivity.java
@@ -0,0 +1,19 @@
+package com.example.java.androidfire.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.java.androidfire.R;
+
+public class ZhuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhu);
+
+
+
+
+    }
+}
